a6566b57b52180c35213eccdeca5f656e0b03331
Caxora <caxora@protonmail.com>
2021-12-15 17:36:27 +0100

    change method to static

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Task {
 
-    public boolean equalStrings(String s1, String s2) {
+    public static boolean equalStrings(String s1, String s2) {
         //fill in body method and correct return statement
 
         return false;
