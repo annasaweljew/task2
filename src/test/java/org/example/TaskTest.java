3a193524ae9f991ccc38b9255b54633ca7fb7543
Anna Saweljew <anna.saweljew@o2.pl>
2021-12-21 20:50:56 +0100

    log test results to file

--- src/test/java/org/example/TaskTest.java
+++ src/test/java/org/example/TaskTest.java
@@ -1,51 +1,72 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TaskTest {
+
+    private final TestLogger logger = new TestLogger();
 
     @Test
     void should_compare_two_identical_strings() {
         String s1 = "konstantynopolitaneczka";
         String s2 = "konstantynopolitaneczka";
-        assertTrue(Task.equalStrings(s1,s2));
+        var result = Task.equalStrings(s1, s2);
+        logger.logSingleTestResult("should_compare_two_identical_strings", result==true);
+        assertTrue(result);
     }
 
     @Test
     void should_compare_two_different_strings() {
         String s1 = "konstantynopolitaneczka";
         String s2 = "some other string";
-        assertFalse(Task.equalStrings(s1,s2));
+        var result = Task.equalStrings(s1,s2);
+        logger.logSingleTestResult("should_compare_two_different_strings", result==false);
+        assertFalse(result);
     }
 
     @Test
     void should_compare_two_empty_strings() {
         String s1 = "";
         String s2 = "";
-        assertTrue(Task.equalStrings(s1,s2));
+        var result = Task.equalStrings(s1,s2);
+        logger.logSingleTestResult("should_compare_two_empty_strings", result==true);
+        assertTrue(result);
     }
 
     @Test
     void should_compare_two_strings_where_one_is_null() {
         String s1 = "konstantynopolitaneczka";
         String s2 = null;
-        assertFalse(Task.equalStrings(s1,s2));
+        var result = Task.equalStrings(s1,s2);
+        logger.logSingleTestResult("should_compare_two_strings_where_one_is_null", result==false);
+        assertFalse(result);
     }
 
     @Test
     void should_compare_empty_string_and_null_string() {
         String s1 = "";
         String s2 = null;
-        assertTrue(Task.equalStrings(s1,s2));
+        var result = Task.equalStrings(s1,s2);
+        logger.logSingleTestResult("should_compare_empty_string_and_null_string", result==true);
+        assertTrue(result);
     }
 
     @Test
     void should_compare_two_null_strings() {
         String s1 = null;
         String s2 = null;
+        var result = Task.equalStrings(s1,s2);
+        logger.logSingleTestResult("should_compare_two_null_strings", result==true);
         assertTrue(Task.equalStrings(s1,s2));
     }
 
+    @AfterAll
+    void logTestResults() {
+        logger.saveResults();
+    }
 }
---
+++ src/test/java/org/example/TestLogger.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+//save test results to txt file
+public class TestLogger {
+
+    private static final String MAIN_TEST_LOG_FILE = "test_summary.txt";
+    private static final String DETAIL_TEST_LOG_FILE = "test_details.txt";
+
+    private final Map<String,Boolean> results = new HashMap<>();
+
+    public void logSingleTestResult(String testName, boolean passed) {
+        results.put(testName,passed);
+    }
+
+    public void saveResults() {
+        try {
+            saveSummaryResult(getFilePath(MAIN_TEST_LOG_FILE));
+            saveDetailedResults(getFilePath(DETAIL_TEST_LOG_FILE));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void saveSummaryResult(Path filePath) throws IOException {
+        boolean overallTestResult = !results.containsValue(false);
+        Files.writeString(filePath,
+                "TASK COMPLETED:" + overallTestResult + "\n");
+        long passedTestsCount = results.entrySet().stream().filter(Map.Entry::getValue).count();
+        Files.writeString(filePath,
+                "PASSED TESTS: " + passedTestsCount + " OF " + results.size() ,
+                StandardOpenOption.APPEND);
+    }
+
+    private void saveDetailedResults(Path filePath) {
+        results.forEach((key, value) -> {
+            try {
+                Files.writeString(filePath,
+                        Instant.now() + ";" + key + ";" + value + "\n",
+                        StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    private static Path getFilePath (String filePath) throws IOException {
+        Path path = Path.of(filePath);
+        if (Files.notExists(path)) Files.createFile(path);
+        return path;
+    }
+
+
+
+}
